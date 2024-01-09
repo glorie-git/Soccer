406ac363153828ab442cb7e25a75d769393215fa
Glorie Ramazani <glorieramazani@gmail.com>
2024-01-08 22:09:59 -0800

    connects to backend with the help mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,24 @@
 import express from "express";
+import mongoose from 'mongoose';
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
 
+// mongo connection to database
+// ** what is a promise
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/soccerDB',{
+    useNewUrlParser: true,
+    UseUnifiedTopology: true
+})
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+
 app.get('/', (req, res) =>
-    res.send(`Our Soccer application is running ${PORT}`)
+    res.send(`Our Soccer application is running on port ${PORT}`)
 )
 
 app.listen(PORT, () =>
