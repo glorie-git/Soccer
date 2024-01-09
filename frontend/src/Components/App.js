3cb95ae5a9d30fd3caf3100cea7519833870837c
Glorie Ramazani <glorieramazani@gmail.com>
2024-01-09 06:19:58 -0800

    refectors code and completes PlayerList component

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,26 +1,62 @@
 // import logo from './logo.svg';
+import React from 'react';
 import PlayerList from './Player/PlayerList';
+import axios from 'axios'
 import PlayerForm from './Player/PlayerForm';
 import PlayerSingle from './Player/PlayerSingle';
 import './App.css';
 
-function App() {
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className='col s12'>Menu</div>
+class App extends React.Component {
+  constructor(props) {
+    super(props); // assigns our props to our contructor
+    this.state = {
+      players: [],
+      currentPlayer: {},
+    }
+
+    this.updateCurrentPlayer = this.updateCurrentPlayer.bind(this); // allows us to use the function in our class
+  }
+
+  componentDidMount(){
+    const url = 'http://localhost:4000/players';
+
+    axios.get(url)
+      .then((Response) => {
+        this.setState({
+          players: Response.data
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  updateCurrentPlayer(item) {
+    this.setState({
+      currentPlayer: item,
+    })
+  }
+
+  render () {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className='col s12'>Menu</div>
+        </div>
+
+        <div className="row">
+          <div className='col s3'>< PlayerList players={this.state.players}
+            updateCurrentPlayer={this.updateCurrentPlayer}/>
+          </div>
+          <div className='col s9'>< PlayerSingle /></div>
+        </div>
+        
+        <div className="row">
+          <div className='col s12'>< PlayerForm /></div>
+        </div>
       </div>
-      <div className="row">
-        <div className='col s3'>< PlayerList /></div>
-      </div>
-      <div className="row">
-        <div className='col s9'>< PlayerSingle /></div>
-      </div>
-      <div className="row">
-        <div className='col s9'>< PlayerForm /></div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
