968fd6a67dfc54d7e8facf5d90063129fc51908e
Glorie Ramazani <glorieramazani@gmail.com>
2024-01-08 22:43:45 -0800

    adds soccerRoutes import and fixes depreication warnings

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from 'mongoose';
 import bodyParser from "body-parser";
+import routes from "./src/routes/soccerRoutes";
 
 const app = express();
 const PORT = 3000;
@@ -9,13 +10,17 @@ const PORT = 3000;
 // ** what is a promise
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/soccerDB',{
-    useNewUrlParser: true,
-    UseUnifiedTopology: true
+    // useNewUrlParser is a deprecated option: useNewUrlParser and useUnifiedTopology has no effect 
+    // since Node.js Driver version 4.0.0 and will be removed in the next major version
+    // useNewUrlParser: true,
+    // UseUnifiedTopology: true
 })
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
+
+routes(app);
 
 app.get('/', (req, res) =>
     res.send(`Our Soccer application is running on port ${PORT}`)
